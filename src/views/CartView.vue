d1e73a35b71d3ff9ac2167f1826b4b7a7775d82d
VitaliiHoida <vitaliy.gojda@gmail.com>
2024-07-09 12:35:06 +0300

    edit back url

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,84 +1,94 @@
 <template>
   <div class="wrap">
-    <h2>Оплата курсу <br> {{ course.title.rendered }}</h2>
+    <h2>
+      Оплата курсу <br />
+      {{ course.title.rendered }}
+    </h2>
     <div class="item_block">
-      <img :src="course.ACF.bot_image" class="course_img"/>
+      <img :src="course.ACF.bot_image" class="course_img" />
       <div class="info">
         <div class="line">
           <div class="text">Тривалість</div>
-          <div class="value"> {{ course.ACF.bot_course_duration }} місяці</div>
+          <div class="value">{{ course.ACF.bot_course_duration }} місяці</div>
         </div>
         <div class="line">
           <div class="text">Вартість за&nbsp;місяць</div>
-          <div class="value"> {{ month }} грн</div>
-
+          <div class="value">{{ month }} грн</div>
         </div>
         <div class="line">
           <div class="text">Повна вартість</div>
           <div class="sale">-10%</div>
           <div class="value">
             <span class="old">{{ fullSum }} грн</span>
-            <br>
+            <br />
             <span>{{ salePrice }} грн</span>
           </div>
         </div>
       </div>
     </div>
-    <button type="button" @click="monthPay" :class="{active: btn1}">За місяць</button>
-    <button type="button" @click="fullPay" :class="{active: btn2}">Повна вартість</button>
+    <button type="button" @click="monthPay" :class="{ active: btn1 }">
+      За місяць
+    </button>
+    <button type="button" @click="fullPay" :class="{ active: btn2 }">
+      Повна вартість
+    </button>
     <div v-if="btn1 || btn2">
       <div class="line_bottom"></div>
       <div class="coupon">
-        <input type="text" placeholder="Промокод" v-model="code">
+        <input type="text" placeholder="Промокод" v-model="code" />
         <span class="hint" v-if="hint">
-          Вітаємо! Промокод введено вірно!<br>
+          Вітаємо! Промокод введено вірно!<br />
           Ваша знижка складає 25%
         </span>
       </div>
     </div>
     {{ pay_link }}
-    <button type="button" @click="goToSite" >Подбробиці на сайті →</button>
-
+    <button type="button" @click="goToSite">Подбробиці на сайті →</button>
   </div>
 </template>
 
 <script>
-import {mapState} from "vuex";
-import {useTelegram} from "@/helpers/useTelegram"
+import { mapState } from "vuex";
+import { useTelegram } from "@/helpers/useTelegram";
 import axios from "@/api/axios";
 
 export default {
-  name: 'CartView',
+  name: "CartView",
   data: () => ({
     order: {
-      course_name: '',
-      sum_to_pay: '',
-      promo_code: '',
-      user_name: '',
-      user_phone: '',
+      course_name: "",
+      sum_to_pay: "",
+      promo_code: "",
+      user_name: "",
+      user_phone: "",
     },
     btn1: false,
     btn2: false,
-    code: '',
-    sum: '',
+    code: "",
+    sum: "",
   }),
   computed: {
-    ...mapState('courses', { selectedCource: 'course', courses: 'data' }),
-    course(){
-      return this.selectedCource ?? this.courses.find(obj => obj.id === Number(this.$route.query.id));
+    ...mapState("courses", { selectedCource: "course", courses: "data" }),
+    course() {
+      return (
+        this.selectedCource ??
+        this.courses.find((obj) => obj.id === Number(this.$route.query.id))
+      );
     },
     month() {
       return this.course.ACF.bot_course_price * 1;
     },
     fullSum() {
-      return (this.course.ACF.bot_course_price * this.course.ACF.bot_course_duration);
+      return (
+        this.course.ACF.bot_course_price * this.course.ACF.bot_course_duration
+      );
     },
     salePrice() {
       return this.fullSum * 0.9;
     },
     hint() {
-      return this.code === 'black friday';
-    }
+      return this.code === "black friday";
+    },
   },
   methods: {
     monthPay() {
@@ -87,7 +97,7 @@ export default {
       this.order.course_name = this.course.title.rendered;
       this.sum = this.month;
       this.order.sum_to_pay = this.sum;
-      this.order.descr = '1 місяць';
+      this.order.descr = "1 місяць";
       this.sendData(this.order);
     },
     fullPay() {
@@ -96,56 +106,55 @@ export default {
       this.order.course_name = this.course.title.rendered;
       this.sum = this.salePrice;
       this.order.sum_to_pay = this.sum;
-      this.order.descr = 'повний курс';
+      this.order.descr = "повний курс";
       this.sendData(this.order);
     },
     sendData(order) {
-      const {tg, queryId, user, chatId} = useTelegram();
-      order.user_name = user.first_name + ' ' + user.last_name;
+      const { tg, queryId, user, chatId } = useTelegram();
+      order.user_name = user.first_name + " " + user.last_name;
       order.photo = this.course.ACF.bot_image;
 
       tg.MainButton.setParams({
-        text: 'Сплатити ' + order.sum_to_pay + ' грн.',
-        color: '#217C2F',
+        text: "Сплатити " + order.sum_to_pay + " грн.",
+        color: "#217C2F",
       });
 
       tg.MainButton.show();
 
       const mainButtonHandler = function () {
-
         const data = {
           order,
           queryId,
-          chatId
+          chatId,
         };
 
-        axios.post('https://wabot.back.staj.fun/web-data', data, {headers: {'Content-Type': 'application/json'}}).then(
-            (response) => {
-              tg.openInvoice(response.data, function (status) {
-                if (status == 'paid') {
-                  tg.close();
-                } else if (status == 'failed') {
-                  tg.HapticFeedback.notificationOccurred('error');
-                  //Cafe.showStatus('Payment has been failed.');
-                } else {
-                  tg.HapticFeedback.notificationOccurred('warning');
-                }
-              });
-            }
-        );
-
+        axios
+          .post("https://wabot.webnauts.academy/web-data", data, {
+            headers: { "Content-Type": "application/json" },
+          })
+          .then((response) => {
+            tg.openInvoice(response.data, function (status) {
+              if (status == "paid") {
+                tg.close();
+              } else if (status == "failed") {
+                tg.HapticFeedback.notificationOccurred("error");
+                //Cafe.showStatus('Payment has been failed.');
+              } else {
+                tg.HapticFeedback.notificationOccurred("warning");
+              }
+            });
+          });
 
         tg.MainButton.hide();
 
-        tg.offEvent('mainButtonClicked', mainButtonHandler);
+        tg.offEvent("mainButtonClicked", mainButtonHandler);
       };
 
-      tg.onEvent('mainButtonClicked', mainButtonHandler);
-
+      tg.onEvent("mainButtonClicked", mainButtonHandler);
     },
     goToSite() {
       window.open(this.course.link, "_blank");
-    }
+    },
   },
   watch: {
     hint() {
@@ -161,9 +170,9 @@ export default {
         this.order.sum_to_pay = this.sum;
         this.sendData(this.order);
       }
-    }
+    },
   },
-}
+};
 </script>
 
 <style scoped>
@@ -227,8 +236,8 @@ h2 {
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: #E00A23;
-  color: #FFFFFF;
+  background-color: #e00a23;
+  color: #ffffff;
   border-radius: 50%;
   font-weight: 700;
   font-size: 12px;
@@ -242,11 +251,16 @@ h2 {
   font-size: 12px;
   line-height: 14px;
   color: #878787;
-  background-image: linear-gradient(to bottom right, transparent 46%, #E00A23, transparent 53%);
+  background-image: linear-gradient(
+    to bottom right,
+    transparent 46%,
+    #e00a23,
+    transparent 53%
+  );
 }
 
 .item_block .info .line:last-of-type {
-  background: #D9D9D933;
+  background: #d9d9d933;
   backdrop-filter: blur(2px);
   padding: 5px 7px 5px 6px;
   align-items: center;
@@ -258,7 +272,8 @@ h2 {
   margin-top: 15px;
 }
 
-button, .site_link {
+button,
+.site_link {
   font-weight: 700;
   font-size: 16px;
   line-height: 19px;
@@ -277,14 +292,14 @@ button, .site_link {
 button:hover,
 button.active,
 .site_link:active {
-  color: #FFFFFF;
-  background-color: #E00A23;
-  border: 1px solid #E00A23;
+  color: #ffffff;
+  background-color: #e00a23;
+  border: 1px solid #e00a23;
   transition: 0.3s ease-in;
 }
 
 .line_bottom {
-  background: #D9D9D9;
+  background: #d9d9d9;
   opacity: 0.7;
   height: 1px;
   width: calc(100% + 30px);
